37d7a7fc593fe58d60df6521d855a9f88aca395b
michaelwatsonphd <46132965+michaelwatsonphd@users.noreply.github.com>
2018-12-25 08:25:17 -0600

    Initial Commit

---
+++ CategoryTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CategoryTest
+//
+//  Created by Michael Watson on 12/25/18.
+//  Copyright © 2018 Michael Watson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ CategoryTestTests/CategoryTestTests.swift
@@ -0,0 +1,34 @@
+//
+//  CategoryTestTests.swift
+//  CategoryTestTests
+//
+//  Created by Michael Watson on 12/25/18.
+//  Copyright © 2018 Michael Watson. All rights reserved.
+//
+
+import XCTest
+@testable import CategoryTest
+
+class CategoryTestTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
